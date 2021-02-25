e96b8d8eb4716df6503d7381c6e259cfa67cdb29
Rafael Costa <rafael.costab@hotmail.com>
2021-02-24 23:07:41 -0300

    criando title e adicionando icone

--- pomodoro/src/pages/index.tsx
+++ pomodoro/src/pages/index.tsx
@@ -1,14 +1,18 @@
-
 import { CompletedChallenges } from '../components/CompletedChallenges';
 import { Countdown } from '../components/CountDown';
 import { ExperienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
+
+import Head from 'next/head';
 
 import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Início | Pomodoro</title>
+      </Head>
       <ExperienceBar/>
 
       <section>
