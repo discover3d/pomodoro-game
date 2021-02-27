10412d6eabb762df9199996f898581cf98c67550
Rafael Costa <rafael.costab@hotmail.com>
2021-02-27 01:02:05 -0300

    estrutura de leveling
    trazendo regras de negócio do countdown para o context para que seja acessada por outros componentes.

--- pomodoro/src/components/Profile.tsx
+++ pomodoro/src/components/Profile.tsx
@@ -1,6 +1,11 @@
-import styles from '../styles/components/Profile.module.css';
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import styles from '../styles/components/Profile.module.css';import { ChallengeBox } from './ChallengeBox';
 
 export function Profile(){
+
+    const { level } = useContext(ChallengesContext);
+
     return (
         <div className={styles.profileContainer}>
             <img src="https:github.com/rafaelcostab.png" alt="Rafael Costa"/>
@@ -8,7 +13,7 @@ export function Profile(){
                 <strong>Rafael Costa</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level"/>
-                    Level 1
+                    Level {level}
                 </p>
             </div>
         </div>  
